e4a361f1c94a315290e8654bc5180a73be0852ac
Joel Trinidad <jth_2929@hotmail.com>
2022-08-25 18:17:53 -0500

    feat: router system added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import { Box, Container } from "@chakra-ui/react";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Surtidor from "./pages/Surtidor"
+import Product from "./pages/Product"
+import MethodPayment from "./pages/MethodPayment"
+import Quantity from "./pages/Quantity"
+import Resume from "./pages/Resume"
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Box w="100%" h="100vh">
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/surtidor" element={<Surtidor />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/method-payment" element={<MethodPayment />} />
+          <Route path="/quantity" element={<Quantity />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </Container>
+    </Box>
   );
 }
 
