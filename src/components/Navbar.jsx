73d462eb88bbec4666cf0e40c29ad41e4586e116
Joel Trinidad <jth_2929@hotmail.com>
2022-08-26 14:50:34 -0500

    feat: Navbar content created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,40 @@
+import { Image, Box, Heading } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import ImageShop from "../assets/img/shop.png";
+
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const redirect = () => {
+    navigate("/");
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <Box
+      display="block"
+      alignItems="center"
+      justifyContent="space-evenly"
+      borderBottom="1px solid #ddd"
+    >
+      <Image
+        src={ImageShop}
+        alt="Logotipo"
+        width={75}
+        margin="0 auto"
+        cursor="pointer"
+        onClick={redirect}
+      />
+      <Heading
+        as="h1"
+        size="sm"
+        textTransform="uppercase"
+        textAlign="center"
+        color="#ff6b6b"
+        fontWeight="900"
+        paddingBottom="5"
+      >
+        Gasolinera Trinidad
+      </Heading>
+    </Box>
+  );
 }
