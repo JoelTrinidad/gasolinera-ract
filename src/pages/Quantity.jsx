17aa4fa6045e56810516002af4de89876d21b39b
Joel Trinidad <jth_2929@hotmail.com>
2022-08-28 18:34:50 -0500

    ·feat: quantity saved

--- src/pages/Quantity.jsx
+++ src/pages/Quantity.jsx
@@ -4,16 +4,28 @@ import { useNavigate } from "react-router-dom";
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Quantity() {
+  const [counter, setCounter] = useState("");
   const navigate = useNavigate();
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  useEffect(() => {
+    localStorage.setItem("quantity", counter);
+  }, [counter]);
+
+  const sumCounter = (number) => {
+    setCounter(counter + number);
+  };
+
+  const clearCounter = () => {
+    setCounter("");
+  };
 
   return (
     <>
       <Grid marginBottom={5}>
         <Heading size="lg">Pulse importe</Heading>
         <Heading size="lg" fontWeight="800">
-          {" "}
-          Contador de dolares
+          {counter ? <>${counter}</> : <>$0</>}
         </Heading>
       </Grid>
       <Grid templateColumns="repeat(3, 1fr)" gap={6}>
@@ -23,6 +35,7 @@ export default function Quantity() {
             w="100%"
             h="20"
             bgGradient="linear(to-r, gray.200, gray.500)"
+            fontSize="40px"
             display="flex"
             alignItems="center"
             justifyContent="center"
@@ -30,7 +43,7 @@ export default function Quantity() {
             boxShadow="0px 4px 10px -3px rgb(117 117 177)"
             cursor="pointer"
             _hover={{ shadow: "2xl" }}
-            onClick={() => console.log(number)}
+            onClick={() => sumCounter(number)}
           >
             {number}
           </GridItem>
@@ -42,6 +55,7 @@ export default function Quantity() {
           h="20"
           bgGradient="linear(to-r, gray.200, gray.500)"
           colSpan={1}
+          fontSize="40px"
           display="flex"
           alignItems="center"
           justifyContent="center"
@@ -49,7 +63,7 @@ export default function Quantity() {
           fontWeight="700"
           boxShadow="0px 4px 10px -3px rgb(117 117 177)"
           _hover={{ shadow: "2xl" }}
-          onClick={() => console.log("0")}
+          onClick={() => sumCounter("0")}
         >
           0
         </GridItem>
@@ -66,7 +80,7 @@ export default function Quantity() {
           fontWeight="700"
           boxShadow="0px 4px 10px -3px rgb(117 117 177)"
           _hover={{ shadow: "2xl" }}
-          onClick={() => console.log("Borrar")}
+          onClick={clearCounter}
         >
           Borrar
         </GridItem>
